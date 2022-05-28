54149e12d47a2a26704a29129fa9144106015d3c
martin stiebing <martin.stiebing@web.de>
2022-05-28 13:38:49 +0200

    character movement and collision and camera move

--- src/main.js
+++ src/main.js
@@ -42,20 +42,20 @@ const Game_map = function(screen, map_data) {
     map.sprite.src = map_data.sprite;
 
     map.scale = 2;
-    map.cell_size = 32 / map.scale;
+    map.cell_size = 32;
     
-    map.width = map_data.width / map.scale;
-    map.height = map_data.height / map.scale;
+    map.width = map_data.width;
+    map.height = map_data.height;
 
     map.pos_x = screen.width / 2 - map.width / 2;
-    map.pos_y = screen.height / 2 - map.height / 2;
+    map.pos_y = screen.height / 2 - map.height;
 
     map.collision_2d = cast_to_2d(map_data.collisions);
-    map.collisions = get_cell_pos(map.collision_2d, screen, map);
-
+    
     map.render = function() {
         screen.ctx.drawImage(map.sprite, map.pos_x, map.pos_y, map.width, map.height);
-
+        map.collisions = get_cell_pos(map.collision_2d, screen, map);
+        
         map.collisions.forEach(cell => {
             screen.ctx.fillStyle = "rgba(200, 0, 0, .3)"
             screen.ctx.fillRect(cell.pos_x, cell.pos_y, cell.size - 2, cell.size - 2)
@@ -68,7 +68,7 @@ const Game_map = function(screen, map_data) {
 const Sprite = function(screen, map) {
     const sprite = {};
     sprite.direction = new Map();
-    sprite.resize = 4;
+    sprite.resize = 5;
 
     sprite.collisions_detection = function(cell) {
         let border_right = sprite.pos_x + sprite.width - sprite.right >= cell.pos_x;
@@ -127,40 +127,65 @@ const Player = function(screen, map) {
     player.data = get_cell_pos(player.position_data, screen, map);
 
     player.data.forEach(data => {
-        player.width = player.height = data.size
+        player.width = player.height = data.size;
         player.pos_x = data.pos_x;
         player.pos_y = data.pos_y;
     });
 
-    player.speed = 3;
+    player.default_speed = 4;
+    player.speed = player.default_speed;
     player.color = "rgba(200, 200, 200, .4)";
 
-    addEventListener("keydown", key => { player.user_input(key.keyCode, true) });
-    addEventListener("keyup", key => { player.user_input(key.keyCode, false) });
+    addEventListener("keydown", key => { player.key_press(key.keyCode) });
+    addEventListener("keyup", key => { player.key_release(key.keyCode) });
 
-
-
-    player.user_input = function(key, value) {
-        if (key == 68) { player.direction.set("right", value) };
-        if (key == 65) { player.direction.set("left", value)};
-        if (key == 83) { player.direction.set("down", value)};
-        if (key == 87) { player.direction.set("up", value)};
+    player.key_press = function(key) {
+        if (key == 68) { player.direction.set("right", true);};
+        if (key == 65) { player.direction.set("left", true);};
+        if (key == 83) { player.direction.set("down", true);};
+        if (key == 87) { player.direction.set("up", true);};
     };
+
+    player.key_release = function(key) {
+        if (key == 68) { player.direction.set("right", false); player.speed = player.default_speed;};
+        if (key == 65) { player.direction.set("left", false); player.speed = player.default_speed;};
+        if (key == 83) { player.direction.set("down", false); player.speed = player.default_speed;};
+        if (key == 87) { player.direction.set("up", false); player.speed = player.default_speed;};
+    }
 
     return player;
 };
 
 const Camera = function(screen, player, map) {
     const camera = {};
-    camera.width = player.width * 15;
-    camera.height = player.height * 10; 
+    camera.width = map.width / 2;
+    camera.height = map.height / 2; 
+    camera.speed = player.default_speed;
 
     camera.render = function() {
         camera.pos_x = player.pos_x - camera.width / 2 + player.width / 2;
         camera.pos_y = player.pos_y - camera.height / 2 + player.height / 2;
 
-        screen.ctx.fillStyle = "rgba(50, 50, 50 .3)";
-        screen.ctx.fillRect(camera.pos_x, camera.pos_y, camera.width, camera.height);
+        if (!player.collide) {
+            if (player.direction.get("right") && player.pos_x > screen.width / 2) { 
+                map.pos_x -= camera.speed; 
+                player.speed = 0;
+            }
+            if (player.direction.get("left") && player.pos_x < screen.width / 2) {
+                map.pos_x += camera.speed;
+                player.speed = 0;
+            }
+            if (player.direction.get("down") && player.pos_y > screen.height / 2) {
+                map.pos_y -= camera.speed;
+                player.speed = 0;
+            }
+            if (player.direction.get("up") && player.pos_y < screen.height / 2) {
+                map.pos_y += camera.speed;
+                player.speed = 0;
+            };
+        };
+        // screen.ctx.fillStyle = "rgba(200, 200, 200 .3)";
+        // screen.ctx.fillRect(camera.pos_x, camera.pos_y, camera.width, camera.height);
     };
 
     return camera;
